130ac53f83833ccd01ba711baf8afc5fcd333d3f
André de Almeida <andre141097@gmail.com>
2019-06-10 14:24:23 -0300

    mandando página home a ser modificada

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+function lampadaAcesa(){
+
+	document.getElementById('lampada').src = "img/simbolo-on.png";
+};
+function lampadaApaga(){
+	document.getElementById('lampada').src = "img/simbolo.png";
+};
+// $(document).ready(function(){
+
+// });
